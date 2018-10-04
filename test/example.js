30ce6d4df068c3c79a4425fe479656e3cd6fc7f7
Taras <php.laboratory@gmail.com>
2018-10-04 16:24:54 +0300

    moving to rate limit

--- test/example.js
+++ test/example.js
@@ -10,12 +10,13 @@ const binanceRest = new binanceApi.BinanceRest({
      * Optional, default is false. Binance's API returns objects with lots of one letter keys.  By
      * default those keys will be replaced with more descriptive, longer ones.
      */
-    handleDrift: false
+    handleDrift: false,
     /* Optional, default is false.  If turned on, the library will attempt to handle any drift of
      * your clock on it's own.  If a request fails due to drift, it'll attempt a fix by requesting
      * binance's server time, calculating the difference with your own clock, and then reattempting
      * the request.
      */
+    limitRate: 3
 });
 
 
